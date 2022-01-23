d714eea13b684fd13f8876ad4714db22d03846e4
95Chester95 <pandastream95@gmail.com>
2022-01-23 18:22:30 +0200

    First Commit

---
+++ calc.js
@@ -0,0 +1,33 @@
+function Calc(operator, a, b) {
+  const isNotValid = typeof a !== 'number' || typeof b !== 'number';
+  if (isNotValid) {
+    return 'Error';
+  }
+  
+  switch (operator) {
+    case 'sum':
+      return a + b;
+      
+    case 'sub':
+      return a - b;
+    
+    case 'multi':
+      return a * b;
+      break;
+    
+    case 'div':
+      return a / b;
+      break;
+
+    case 'rem':
+      return a % b;
+
+    case 'pow':
+      return a ** b;
+    
+    default :
+      return 'unknown operation';
+  }
+}
+
+console.log(Calc('div', 4, 5));
