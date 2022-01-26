f9e22c131e747d2d43e4ea9f0508b629f4037ec5
95Chester95 <pandastream95@gmail.com>
2022-01-26 20:22:55 +0200

    Calc in object

--- calc.js
+++ calc.js
@@ -1,34 +1,66 @@
+// function Calc(operator, a, b) {
+//   const isNotValid = typeof a !== 'number' || typeof b !== 'number';
+//   if (isNotValid) {
+//     return 'Error';
+//   }
+  
+//   switch (operator) {
+//     case 'sum':
+//       return a + b;
+      
+//     case 'sub':
+//       return a - b;
+    
+//     case 'multi':
+//       return a * b;
+//       break;
+    
+//     case 'div':
+//       return a / b;
+//       break;
+
+//     case 'rem':
+//       return a % b;
+
+//     case 'pow':
+//       return a ** b;
+    
+//     default :
+//       return 'unknown operation';
+//   }
+// }
+
+// console.log(Calc('div', 4, 5));
+// console.log(Calc('pow', 3, 7));
+
+
+
+
+
+
 function Calc(operator, a, b) {
   const isNotValid = typeof a !== 'number' || typeof b !== 'number';
   if (isNotValid) {
     return 'Error';
   }
   
-  switch (operator) {
-    case 'sum':
-      return a + b;
-      
-    case 'sub':
-      return a - b;
-    
-    case 'multi':
-      return a * b;
-      break;
-    
-    case 'div':
-      return a / b;
-      break;
+  let operation = {
+    sum: a + b,
+    sub: a - b,
+    multi: a * b,
+    div: a / b,
+    rem: a % b,
+    pow: a **  b,
+  }
 
-    case 'rem':
-      return a % b;
-
-    case 'pow':
-      return a ** b;
-    
-    default :
-      return 'unknown operation';
+  if ( operator in operation) {
+    return operation[operator] ;
+  } else {
+    return 'unknown operation';
   }
 }
 
-console.log(Calc('div', 4, 5));
-console.log(Calc('pow', 3, 7));
+
+
+console.log(Calc( '', 4, 2));
+console.log(Calc('multi', 3, 7));
